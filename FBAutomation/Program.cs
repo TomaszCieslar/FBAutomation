4dbc74c04c1087a789ab098a55caa17f9185680c
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2020-06-18 05:20:06 +0200

    Sellenium connection

--- FBAutomation/Program.cs
+++ FBAutomation/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace FBAutomation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitiateAutomation();
+
+           // IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory());
+           // driver.Url = "https://www.facebook.com/groups/2143039459260122/members/";
+           
+
+        }
+
+        static void InitiateAutomation()
+        {
+            //Kill Processes
+            Process[] chromedriverProcesses = Process.GetProcessesByName("chromedriver");
+            foreach (var process in chromedriverProcesses)
+
+            {
+                Console.WriteLine(process.ProcessName);
+            }
         }
     }
 }
