cf974f3e9aa71d707f3dafd8d6f803fe608681ef
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2020-06-18 04:40:32 +0200

    Initial pull

---
+++ FBAutomation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FBAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
